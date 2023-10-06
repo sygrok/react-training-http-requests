dfb7d847540ce06ec6398fec79ffc27244b0dd64
Berkay Anduv <sygrok@hotmail.com>
2023-10-06 13:36:32 +0300

    HandlingErrors

--- src/App.js
+++ src/App.js
@@ -5,22 +5,47 @@ import MoviesList from "./components/MoviesList";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   async function fetchMoviesHandler() {
     setIsLoading(true);
-    const response = await fetch("https://swapi.py4e.com/api/films/");
-    const data = await response.json();
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/films/");
 
-    const transformedData = data.results.map((x) => {
-      return {
-        id: x.episode_id,
-        title: x.title,
-        openingText: x.opening_crawl,
-        releaseDate: x.release_date,
-      };
-    });
-    setMovies(transformedData);
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+
+      const transformedData = data.results.map((x) => {
+        return {
+          id: x.episode_id,
+          title: x.title,
+          openingText: x.opening_crawl,
+          releaseDate: x.release_date,
+        };
+      });
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (!isLoading && movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
   }
 
   return (
@@ -28,11 +53,7 @@ function App() {
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <h2>Found no movies</h2>}
-        {isLoading && <h1>Loading...</h1>}
-      </section>
+      <section>{content}</section>
     </>
   );
 }
