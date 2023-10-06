deaede2f5b9e155109a0db382681e8edafa55169
Berkay Anduv <sygrok@hotmail.com>
2023-10-06 12:57:29 +0300

    Loading

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import MoviesList from "./components/MoviesList";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setIsLoading(true);
     const response = await fetch("https://swapi.py4e.com/api/films/");
     const data = await response.json();
 
@@ -18,6 +20,7 @@ function App() {
       };
     });
     setMovies(transformedData);
+    setIsLoading(false);
   }
 
   return (
@@ -26,7 +29,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && <h2>Found no movies</h2>}
+        {isLoading && <h1>Loading...</h1>}
       </section>
     </>
   );
